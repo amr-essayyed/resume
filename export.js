2bccc3d59e6e4cdaabd187e78b42b54d0b2d71e3
Amr Sam <amr1997sam@gmail.com>
2025-05-01 00:48:21 +0300

    over chromium security feature

--- export.js
+++ export.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     await page.goto('file://' + __dirname + '/index.html', { waitUntil: 'networkidle0' });
     await page.pdf({ path: 'Amr_Essayyed_CV.pdf', format: 'A4' });
